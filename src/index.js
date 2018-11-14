aa102d4ecd69202cdcee7857046931f3c2f768b5
Todd Rasband <todd.rasband@gmail.com>
2018-11-14 16:43:15 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './styles/main.css';
+import './styles/main.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
